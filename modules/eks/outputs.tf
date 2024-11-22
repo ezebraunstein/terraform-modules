2147dd5b15177c587828855d125af15021084b3c
ezequiel <ezequiel.braunstein@gmail.com>
2024-11-22 14:06:26 -0300

    :lipstick:Modified EKS role creation

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -3,16 +3,6 @@ output "cluster_name" {
   value       = aws_eks_cluster.eks_cluster.name
 }
 
-output "cluster_role_arn" {
-  description = "The ARN of the EKS cluster role"
-  value       = aws_iam_role.eks_cluster_role.arn
-}
-
-output "node_role_arn" {
-  description = "ARN of the IAM role for EKS nodes"
-  value       = aws_iam_role.eks_node_role.arn
-}
-
 output "node_group_name" {
   description = "The name of the EKS node group"
   value       = aws_eks_node_group.eks_node_group.node_group_name
