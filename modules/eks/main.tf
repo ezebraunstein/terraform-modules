81ceb0db9e585f5974229fe7fa08084b97774e51
ezequiel <ezequiel.braunstein@gmail.com>
2024-11-22 12:47:53 -0300

    :lipstick:Modified EKS node group

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -84,7 +84,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = var.node_role_arn
-  subnets         = var.subnet_ids
+  subnet_ids        = var.subnet_ids
 
   scaling_config {
     desired_size = var.desired_size
